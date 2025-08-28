84dda895c67905af5a0e8024868c75c20a809e89
Ivanna-Kutsovol <st6815720@stud.duikt.edu.ua>
2025-08-28 23:07:09 +0200

    feat(product-page): add scroll/resize handlers, tab click, and responsive styles <425px

--- src/app/catalogy/[id]/page.tsx
+++ src/app/catalogy/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, use } from "react";
+import React, { useState, use, useRef, useEffect } from "react";
 import { products } from "../../../data/product";
 import Image from "next/image";
 import stl from "./page.module.scss";
@@ -26,25 +26,85 @@ const Product = ({ params, images }: { params: Promise<{ id: string }>, images: 
     const product = products.find((p) => p.id === Number(id));
     const [activeTab, setActiveTab] = useState("info");
     const [thumbsSwiper, setTrumbsSwiper] = useState<SwiperType | null>(null);
+    const [widthWindow, setWidthWindow] = useState(0);
+
+    const parentRef = useRef<HTMLDivElement | null>(null);
+    const buttonRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const parent = parentRef.current;
+        const button = buttonRef.current; 
+
+        if(!parent || !button) return;
+
+        const handlyScroll = () => {
+
+            const rect = parent.getBoundingClientRect();
+
+            if(rect.bottom > window.innerHeight){
+                button.style.display = "block";
+                button.style.position = "fixed";
+                button.style.bottom = "0";
+            } else {
+                button.style.display = "none";
+            }
+        }
+
+        window.addEventListener("scroll", handlyScroll);
+        handlyScroll();
+        return () => window.removeEventListener("scroll", handlyScroll);
+
+    },[])
+
+    useEffect(() => {
+        if (typeof window === "undefined") return;
+
+        const handeResize = () => setWidthWindow(window.innerWidth);
+
+        handeResize();
+        window.addEventListener("resize", handeResize);
+
+        return () => window.removeEventListener("resize", handeResize);
+    }, [])
+
+    const tabsContainerRef = useRef<HTMLDivElement>(null)
+
+    const handlyClick = (tab: string, index: number) => {
+        setActiveTab(tab);
+
+        if(tabsContainerRef.current){
+            const tabNode = tabsContainerRef.current.children[index] as HTMLElement;
+            const container = tabsContainerRef.current;
+
+            const containerRect = container.getBoundingClientRect();
+            const tabRect = tabNode.getBoundingClientRect();
+
+            container.scrollLeft += tabRect.left - containerRect.left - (containerRect.width / 2 - tabRect.width / 2);
+        }
+    }
 
     if(!product) return <h1>Product not found</h1>;
 
     const quantity = cart[product.id] || 0;
 
     return(
-        <section className={stl.container}>
+        <section className={stl.container} ref={parentRef}>
             <Link className={stl.back} href="/">Back</Link>
+            {widthWindow < 425 && (
+                <h1 className={stl.product__title}>{product.name}</h1>
+            )}
             <section className={stl.product}>
                 <div className={stl.product__imgContainer}>
                     <Swiper
                         modules={[Navigation, Autoplay, Thumbs]}
                         autoplay={{ delay: 3000 }}
                         spaceBetween={30}
-                        thumbs={{ swiper: thumbsSwiper }}
+                        thumbs={{ swiper: widthWindow >= 425 ? thumbsSwiper : null }}
                         centeredSlides={true}
                         loop={true}
                         className={stl.swiper}
-                        > 
+                        navigation={widthWindow < 425 ? true : false}
+                    > 
                         {product.details.img.map((img, index) => (
                             <SwiperSlide key={`main-${index}`}>
                                 <Image className={stl.product__imgContainer__mainImg} src={img} alt={`product-${index}`} width={300} height={427} />
@@ -70,30 +130,69 @@ const Product = ({ params, images }: { params: Promise<{ id: string }>, images: 
                         ))}
                     </Swiper>
                 </div>
-                <div className={stl.product__info}>
-                    <div className={stl.product__titlePrice}>
-                        <h1 className={stl.product__title}>{product.name}</h1>
-                        <h2 className={stl.product__price}>$ {product.price}</h2>
+                {widthWindow <= 425 ? (
+                    <div className={stl.product__infoContainer}>
+                        <div className={stl.product__info}>
+                            <div className={stl.product__titlePrice}>
+                                <h2 className={stl.product__price}>$ {product.price}</h2>
+                            </div>
+
+                        <div className={stl.product__counter}>
+                            {quantity >= 0 && (
+                                <div className={stl.product__counter}>
+                                    <button className={stl.product__counter__button} onClick={() => updateQuantity(product.id, quantity - 1)}>−</button>
+                                    <p className={stl.product__counter__text}>{quantity}</p>
+                                    <button className={stl.product__counter__button} onClick={() => updateQuantity(product.id, quantity + 1)}>+</button>
+                                </div>
+                            )}
+                        </div>
                     </div>
 
-                    <div className={stl.product__counter}>
-                        {quantity >= 0 && (
-                            <div className={stl.product__counter}>
-                                <button className={stl.product__counter__button} onClick={() => updateQuantity(product.id, quantity - 1)}>−</button>
-                                <p className={stl.product__counter__text}>{quantity}</p>
-                                <button className={stl.product__counter__button} onClick={() => updateQuantity(product.id, quantity + 1)}>+</button>
-                            </div>
-                        )}
-                    </div> 
                     <div className={stl.product__buttons}>
+                        <Link href="/cart" className={stl.product__buttonCart}><button style={{fontWeight:'bolder', fontSize: '16px'}} onClick={() => addToCart(product.id)}>Add To Cart</button></Link>
+                    </div>
+                    <div className={stl.tabs}>
+                        <div className={stl.tabs__item} ref={tabsContainerRef}>
+                            <p className={`${activeTab === 'info' ? stl.tabs__active : stl.tabs__inactive}`} onClick={() => handlyClick("info", 0)}>Product Info</p>
+                            <p className={`${activeTab === 'spec' ? stl.tabs__active : stl.tabs__inactive}`} onClick={() => handlyClick("spec", 1)}>General Specification</p>
+                            <p className={`${activeTab === 'delivery' ? stl.tabs__active : stl.tabs__inactive}`} onClick={() => handlyClick("delivery", 2)}>Delivery & Returns</p>
+                        </div>
+
+                            <div className={stl.tabs__text}>
+                            {activeTab === 'info' && 
+                                <p >{product.details.fullDescription.productInfo}</p>}
+                                {activeTab === 'spec' && 
+                                <p >{product.details.fullDescription.general}</p>}
+                                {activeTab === 'delivery' &&
+                                <p >{product.details.fullDescription.delivery}</p>}
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    <div className={stl.product__info}>
+                        <div className={stl.product__titlePrice}>
+                            <h1 className={stl.product__title}>{product.name}</h1>
+                            <h2 className={stl.product__price}>$ {product.price}</h2>
+                        </div>
+
+                        <div className={stl.product__counter}>
+                            {quantity >= 0 && (
+                                <div className={stl.product__counter}>
+                                    <button className={stl.product__counter__button} onClick={() => updateQuantity(product.id, quantity - 1)}>−</button>
+                                    <p className={stl.product__counter__text}>{quantity}</p>
+                                    <button className={stl.product__counter__button} onClick={() => updateQuantity(product.id, quantity + 1)}>+</button>
+                                </div>
+                            )}
+                        </div>
+                        <div className={stl.product__buttons}>
                         <Link href="/cart"><button className={stl.product__buttonBuy}  onClick={() => addToCart(product.id)}>Buy it now</button></Link>
                         <Link href="/cart"><button className={stl.product__buttonCart}  onClick={() => addToCart(product.id)}>Add To Cart</button></Link>
                     </div>
                     <div className={stl.tabs}>
-                        <div className={stl.tabs__item}>
-                            <p className={`${activeTab === 'info' ? stl.tabs__active : stl.tabs__inactive}`} onClick={() => setActiveTab("info")}>Product Info</p>
-                            <p className={`${activeTab === 'spec' ? stl.tabs__active : stl.tabs__inactive}`} onClick={() => setActiveTab("spec")}>General Specification</p>
-                            <p className={`${activeTab === 'delivery' ? stl.tabs__active : stl.tabs__inactive}`} onClick={() => setActiveTab("delivery")}>Delivery & Returns</p>
+                        <div className={stl.tabs__item} ref={tabsContainerRef}>
+                            <p className={`${activeTab === 'info' ? stl.tabs__active : stl.tabs__inactive}`} onClick={() => handlyClick("info", 0)}>Product Info</p>
+                            <p className={`${activeTab === 'spec' ? stl.tabs__active : stl.tabs__inactive}`} onClick={() => handlyClick("spec", 1)}>General Specification</p>
+                            <p className={`${activeTab === 'delivery' ? stl.tabs__active : stl.tabs__inactive}`} onClick={() => handlyClick("delivery", 2)}>Delivery & Returns</p>
                         </div>
 
                         <div className={stl.tabs__text}>
@@ -105,12 +204,17 @@ const Product = ({ params, images }: { params: Promise<{ id: string }>, images: 
                             <p >{product.details.fullDescription.delivery}</p>}
                         </div>
                     </div>
-                    
-                </div>
-                
+                    </div>
+                )}
             </section>
-            
-            
+            {widthWindow < 425 && 
+            <div ref={buttonRef} className={stl.product__fixedBtnContainer}>
+                <Link href="/cart" className={stl.product__fixedBtn}>
+                    <button className={stl.product__buttonBuy} onClick={() => addToCart(product.id)}>
+                        <h2 className={stl.product__price}>$ {product.price}</h2> Buy it now</button>
+                </Link>    
+            </div>
+            }
         </section>
         
     )
