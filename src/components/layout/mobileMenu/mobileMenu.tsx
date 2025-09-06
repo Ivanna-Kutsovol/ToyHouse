7516cbcf8eb5b337d5c242716000d1424af431cd
Ivanna-Kutsovol <st6815720@stud.duikt.edu.ua>
2025-09-07 00:30:39 +0200

    fix: mobile navigation links scroll with scroll-padding-top and smooth behavior

--- src/components/layout/mobileMenu/mobileMenu.tsx
+++ src/components/layout/mobileMenu/mobileMenu.tsx
@@ -14,16 +14,16 @@ const MobileMenu:React.FC<MobileMenuProps> = ({  tooggleMenu }) => {
             <nav className={stl.nav} onClick={tooggleMenu}>
                 <ul className={stl.nav__list}>
                     <li className={stl.nav__item}>
-                        <Link className={stl.nav__link} href="/about">About us</Link>
+                        <Link className={stl.nav__link} href="/#about">About us</Link>
                     </li>
                     <li className={stl.nav__item}>
-                        <Link className={stl.nav__link} href="/catalogy">Catalogy</Link>
+                        <Link className={stl.nav__link} href="/#catalogy">Catalogy</Link>
                     </li>
                     <li className={stl.nav__item}>
-                        <Link className={stl.nav__link} href="/feedback">Feedback</Link>
+                        <Link className={stl.nav__link} href="/#feedback">Feedback</Link>
                     </li>
                     <li className={stl.nav__item}>
-                        <Link className={stl.nav__link} href="/contacts">Contacts</Link>
+                        <Link className={stl.nav__link} href="/#contacts">Contacts</Link>
                     </li>
                     <li className={stl.nav__item}>
                         <Link className={stl.nav__linkImg} href="/cart">
